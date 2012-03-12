9e4a0f6530fc57696e3e5390a725d8a3b57c2dc9
Jan Gentsch <jan.gentsch@oose.de>
2012-03-12 15:01:25 +0100

    Initialer Import. Grundgerüst der Anwendung.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuickBacklog::Application.config.secret_token = 'f4781feda2d59a8fa1fbe1cfa60c9fcc4d12abc1c07254bbe573350d8d24e9f9544c3a01a9d607f345f428a5119de546368ec27d663dd33a06e77e7d4edf8142'
