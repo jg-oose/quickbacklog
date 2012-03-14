4aed58395ed42b754d557b14985d1d381f132090
Jan Gentsch <jan.gentsch@oose.de>
2012-03-14 16:51:24 +0100

    Scale als Text-Zeile. Styling Update für Projekt-Form.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,10 +1,34 @@
 class Project < ActiveRecord::Base
   has_many :backlog_entries, :order => :position
-  serialize :estimation_scale, Hash
+  serialize :size_scale, Hash
 
   validates :name, :presence => true 
   
   def new_backlog_entry_from_template
     backlog_entries.build(:description => self.entry_template_text)
+  end
+  
+  def scale_textline
+    size_scale.inject("") do |memo, e|
+      memo += ", " unless memo.empty?
+      memo += "#{e[0]}=#{e[1]}" unless e[0].nil?
+      memo
+    end
+  end
+  
+  def scale_textline=(line)
+    scale = {}
+    r = /([[:alnum:]]+)=([[:digit:]]+),? */
+    line.scan(r) do |k, v|
+      scale.store k.to_s, v.to_i
+    end
+
+    if scale.empty?
+      line.scan(/([[:digit:]]+),? */) do |v, so_it_doesnt_pass_the_match_obj|
+        scale.store v.to_s, v.to_i
+      end
+    end
+
+    self.size_scale = scale
   end
 end
