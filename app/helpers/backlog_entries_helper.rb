a1e67c1432df2c66fef632aa72253fa2f25d1441
Jan Gentsch <jan.gentsch@oose.de>
2012-03-16 16:54:05 +0100

    BacklogEntries.index internationalisiert.

--- app/helpers/backlog_entries_helper.rb
+++ app/helpers/backlog_entries_helper.rb
@@ -1,2 +1,9 @@
 module BacklogEntriesHelper
+  def category_of entry
+    category = entry.category
+    if category.nil? || category.blank?
+      category = I18n.t("backlog_entries.backlog_entry.no_category")
+    end
+    category
+  end
 end
