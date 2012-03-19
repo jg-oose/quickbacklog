f5c86833ea9afbfbca00bc4e66daf5a68b2ea72b
Jan Gentsch <jan.gentsch@oose.de>
2012-03-19 09:16:03 +0100

    Anlegen eines neuen Projekts, wenn noch keines vorhanden ist, um seeding nicht zu benötigen.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,16 @@ class ApplicationController < ActionController::Base
   end
   
   def project_from_session
-    Project.first
+    prj = Project.first
+    unless prj
+      prj = Project.create(
+        name: "Dein naechstes Projekt!",
+        vision: "Beschreib die Essenz Deines Projekt in wenigen Worten ... Template",
+        size_scale: {
+          "0" => 0, "1" => 1, "2" => 2, "3" => 3, "5" => 5,
+          "8" => 8, "13" => 13, "21" => 21, "40" => 40, "100" => 100},
+        entry_template_text: "_Story_\n...\n\n_Howto Demo_\n...\n\n_Value_\n...\n\n_Risks_\n...\n\n_What else you need_...")
+    end
+    return prj
   end
 end
